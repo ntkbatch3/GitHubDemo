b77af61acab44bdd37941cd2dbf7fb6114efea60
ntkbatch3 <81076004+ntkbatch3@users.noreply.github.com>
2021-03-21 13:43:11 -0400

    Update AutoDemo.java

--- src/demo/AutoDemo.java
+++ src/demo/AutoDemo.java
@@ -4,6 +4,7 @@ public class AutoDemo {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("This commit is done by different automation team member");
 		
 		System.out.println("Hello World");
 	}
